ffdc13acf0bc77b86b2c5bcbca0325859bf30f52
pasu <zhangztsysu@gmail.com>
2026-06-28 23:28:56 +0800

    Deploy static site

---
+++ _next/static/p7IBjfJF0WRaW64RWjBu_/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/xiaodong//_next/:path+",
+        "destination": "/xiaodong/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
